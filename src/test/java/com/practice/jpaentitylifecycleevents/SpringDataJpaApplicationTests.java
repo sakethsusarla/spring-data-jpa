49a01476db453abf1be01c27eb2dbaace844be87
Saketh Susarla <sakethsusarla@gmail.com>
2025-01-30 15:13:20 +0530

    initial commit

--- src/test/java/com/practice/jpaentitylifecycleevents/SpringDataJpaApplicationTests.java
+++ src/test/java/com/practice/jpaentitylifecycleevents/SpringDataJpaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.practice.springdatajpa;
+package com.practice.jpaentitylifecycleevents;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
