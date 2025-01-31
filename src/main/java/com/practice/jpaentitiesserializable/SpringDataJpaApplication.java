9b9b820a735ac7c91b8109e034b94db4cbfd7e4a
Saketh Susarla <sakethsusarla@gmail.com>
2025-01-31 16:11:24 +0530

    initial commit

--- src/main/java/com/practice/jpaentitiesserializable/SpringDataJpaApplication.java
+++ src/main/java/com/practice/jpaentitiesserializable/SpringDataJpaApplication.java
@@ -1,4 +1,4 @@
-package com.practice.springdatajpa;
+package com.practice.jpaentitiesserializable;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
